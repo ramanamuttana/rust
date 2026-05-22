5217b6819bcefd0403c09f6a4799efd7fe18295e
Venkata Muttana <ramanamuttana@gmail.com>
2026-05-22 14:01:37 +0200

    Add basic Rust program with assertion and print

---
+++ helloworld.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x: i32 = 5;   // Declare and initialize x
+    let _y: i32;       // Declare but don't initialize _y
+    assert_eq!(x, 5);  // Assert x equals 5
+    println!("Success!");
+}
